10c7c70db5e968108dd8530e26a7e25ed8453e2c
CTS\2162162 <sylvain.boulloud@cognizant.com>
2024-02-06 16:56:31 +0100

    hello.py fixed

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# gitRepo
 Hello
+hello.py added to say hello
+hello.py modified to create a directory and hotfix of it
--- hello.py
+++ hello.py
@@ -1,4 +1,8 @@
 import os
 
-print("Hello")
-os.mkdir("./pythonRepo")
+print("Hello, this script to create folder in current directory.")
+name = str(input("Choose your repo name (must be written in double quotes) : "))
+location = os.getcwd()
+print(name)
+os.mkdir("./"+name)
+print(name + " directory created in this location : " + location)
